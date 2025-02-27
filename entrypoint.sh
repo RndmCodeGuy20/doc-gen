e34b0993219fff365f81bca95b3702dff3d2dcc0
rndmcodeguy <shantanu.mane.200@outlook.com>
2025-02-27 16:50:15 +0530

    fix: removed open api keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,8 +15,7 @@ else
   COMMIT_MESSAGES=$(curl -s -H "Authorization: token $GITHUB_TOKEN" $API_URL | jq -r '.[].commit.message' | tr '\n' '; ')
 fi
 
-export OPENAI_API_KEY="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"
-export GENAI_API_KEY="AIzaSyBx7gXn-sX8oDVWdpORBXsMrXZ9DngSgg0"
+export GENAI_API_KEY=$GENAI_API_KEY
 
 # Run Python script to generate release notes
 # if [ "$DRY_RUN" = "true" ]; then
